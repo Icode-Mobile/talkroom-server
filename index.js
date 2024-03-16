47fbd3aea67da1fc7ab2ac5fb1f8178e8462c193
Caio18-cosenza <caiiocosenza2020@gmail.com>
2024-03-16 11:34:39 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+let PORT = process.env.PORT || 3000;
+
+let users = [];
+let messages = [];
+
+io.on('connection', (socket) => {
+  console.log('Novo usuário conectado', socket.id);
+
+  socket.on('disconnect', () => {
+    console.log('Usuário desconectado!');
+  });
+
+  socket.on('join room', (room) => {
+    socket.join(room);
+
+    const userRoom = users.find((user) => user.room == room);
+    if (userRoom) {
+      userRoom.socket_id = socket.id;
+    } else {
+      users.push({
+        room,
+        socket_id: socket.id,
+      });
+    }
+  });
+
+  socket.on('message', (data) => {
+    const { room, message, author } = data;
+    const msg = {
+      room,
+      text: message,
+      author,
+    };
+    messages.push(msg);
+    io.to(room).emit('message', msg);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log('Server is running!');
+});
